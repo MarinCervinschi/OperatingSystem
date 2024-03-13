834fc575ac7322a1ab07c1f1292f45efdfbb0bb0
MarinCervinschi <marincervinschi3@gmail.com>
2024-03-13 11:58:26 +0100

    lecture wen.13.03.2024

---
+++ shell/mer.13.03.2024/prova.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+echo Il valore della variabile a è $a
+echo Il valore del comando PATH $PATH
+echo Il valore del comando HOME $HOME
---
+++ shell/mer.13.03.2024/prova1.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+echo Il valore della variabile a è $a
+echo Il valore del commando PATH $PATH
+echo Il valore del commando HOME $HOME
+a=hello
+echo Il valore della variabile a ora è $a
