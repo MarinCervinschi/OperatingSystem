435c9ccd5ce77f5a1cc5bdf4b85681c448c761f0
MarinCervinschi <marincervinschi3@gmail.com>
2024-04-09 18:09:01 +0200

    udate exam 22/04/2021

--- shell/exams/22.04.2021/FCR.sh
+++ shell/exams/22.04.2021/FCR.sh
@@ -8,11 +8,27 @@ T=$3
 # mi sposto nella gerarchia
 cd $G
 
-# controllo se esiste un file che termini in .$S
-if test -f *.$S
+#variabile che ci servira' per capire se abbiamo trovato almeno un file
+trovato=false 
+
+for F in *
+do
+    if test -f $F #se è un file
+    then
+        # controlliamo se termina per la stringa cercata
+        case $F in
+        #se il nome rispetta la specifica, settiamo trovato a true
+        *.$S)   trovato=true; break;; 
+        *) ;;
+        esac
+    fi
+done
+
+#se abbiamo trovato almeno un file dobbiamo salvare il nome della dir corrente nel file temporaneo
+if test $trovato = true 
 then
-    # aggiungo la directory nel file tmp
-    pwd >> $T
+    #dobbiamo scrivere il nome della directory nel file temporaneo
+    pwd >> $3
 fi
 
 # chiamata ricorsiva per le sotto-directory della gerarchia
