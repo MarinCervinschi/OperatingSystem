27a9b466eae9daa5ba28e96b93bf8119ab9e9b3f
MarinCervinschi <marincervinschi3@gmail.com>
2024-04-11 12:58:47 +0200

    update

--- shell/exams/29.04.2011/FCR.sh
+++ shell/exams/29.04.2011/FCR.sh
@@ -11,16 +11,12 @@ cd $G
 # fase A
 if test "$fase" = A
 then
-    for i in *
-    do
-        # se file e se il nome = al nome del File passato come parametro
-        if test -f $F
-        then
-            echo `pwd`/$i # stampo il nome assoluto
-            pwd/$i >> $T # lo inserisco nel file globale
-        fi
-    done
-
+    # se file e se il nome = al nome del File passato come parametro
+    if test -f $F
+    then
+        echo "Trovato $2 in `pwd`"
+        pwd/$i >> $T # lo inserisco nel file globale
+    fi
 fi
 
 # fase B
@@ -28,15 +24,11 @@ if test "$fase" = B
 then
     # variabile per controllare se ho trovato il file di nome F
     trovato=false
-    for i in *
-    do
-        if test -f $F
-        then
-            trovato=true
-            break
-        fi
-    done
-
+    if test -f $F
+    then
+        trovato=true
+    fi
+    
     if test trovato = false
     then
         echo la directory `pwd` non contiene file giusti
