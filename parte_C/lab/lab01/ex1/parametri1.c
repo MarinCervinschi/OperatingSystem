72849bb287df06e46986f069054f56f14be6e7eb
MarinCervinschi <marincervinschi3@gmail.com>
2024-04-26 13:37:25 +0200

    comments

--- parte_C/lab/lab01/ex1/parametri1.c
+++ parte_C/lab/lab01/ex1/parametri1.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> /* per exit() */
 
 int main(int argc, char const *argv[]) {
-    
+    /* controllo il numero di parametri */
     if (argc < 2) {
         printf("Error: numero di parametri errato\n");
         exit(1);
     }
 
+    /* stampo il numero totale di parametri e il nome del eseguibile */
     printf("Il programma %s ha %d parametri\n", argv[0], argc - 1);
+
+    /* itero su tutti i parametri e li stampo, indicando l'indice */
     for (int i = 0; i < argc - 1; i++) {
         printf("Parametro %d: %s\n", i, argv[i + 1]);
     }
